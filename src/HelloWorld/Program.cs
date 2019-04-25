38cbdb71f71150ebef7682acd913058d61436c7f
guodp <guodp9u0@gmail.com>
2019-04-25 10:46:13 +0800

    hello world

---
+++ src/HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HelloWorld {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+
+        private void Call() {
+            string input = "";
+            Task.Factory.StartNew(() =>
+            {
+                while (input != "Q") {
+                    Console.WriteLine("Hello World");
+                    Thread.Sleep(1000);
+                }
+            });
+            while (input != "Q") {
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
